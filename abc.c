d080d67541fa327332336c6b564cccd674941727
Your Name <you@example.com>
2024-11-19 09:51:14 +0530

    first commit

---
+++ abc.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a=435,y,z,x;
+printf("enter the number");
+scanf("%d",&a);
+y=a%10;
+printf("unit place digit is %d\n",y);
+return 0;
+}
+
---
+++ test.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+int x;
+printf("enter the number");
+scanf("%d",&x);
+{
+if(x>0)
+printf("the number is positive");
+else
+printf("the number is negative");
+}
+return 0;
+}
+
