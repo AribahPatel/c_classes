703d6d5a13355d207945b039824841d9febd9cd8
Your Name <you@example.com>
2024-11-21 09:42:55 +0530

    addition of digits of given integer

--- abc.c
+++ abc.c
@@ -1,11 +1,29 @@
 #include<stdio.h>
 int main()
 {
-int a=435,y,z,x;
+int x=435,y,z;
 printf("enter the number");
-scanf("%d",&a);
-y=a%10;
+scanf("%d",&x);
+
+y=x%10;
+
+z=x/10;
+int a =z%10;
+
+
+int g=z/10; 
+int b=g%10;
+
+
 printf("unit place digit is %d\n",y);
+printf("tens place digit is %d\n",a);
+printf("hundreds place digit is %d\n",b);
+
+int i=y+a+b;
+printf("addition of those numbers is %d\n",i);
+
+
+
 return 0;
 }
 
